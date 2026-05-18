c5cbc0dd4182b18985b9462176e9cc5ec5830432
Gaby Nieva <gaby.nieva@outlook.com>
2026-05-18 09:47:29 -0600

    Método multiplicar

--- AdiosJava.java
+++ AdiosJava.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 public class AdiosJava {
+    public static int multiplicar(int n1, int n2){
+        return n1 * n2;
+    }
+
     public static void main(String[] args){
         int numero1, numero2;
         Scanner scanner = new Scanner(System.in);
